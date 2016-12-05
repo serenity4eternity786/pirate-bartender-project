5af58c5ad2cc87e3e3152ff7ffb3031c5e63eb6f
serenity4eternity786 <serenity4eternity786@gmail.com>
2016-12-04 22:35:14 -0600

    editing of the comments

--- app.js
+++ app.js
@@ -16,25 +16,19 @@ var Order = function (orderValues) {
     this.fruity = orderValues[4];
 };
 
-//define the drink object based on the pantry one and the drink order defined bellow
 var Drink = function (pantry, drinkOrder) {
     var ingredientNumber = [];
     var ingredientsArray = [];
 
     for (var userPreference in drinkOrder) {
-        //generate a random number in order to pick up on ingredient in each ingredients category  (for example choose "slug of whisky" for a dring where the client selected the "strong" category of ingredients)
         ingredientNumber = generateRandomNumber(0, 2);
         if (drinkOrder[userPreference]) {
-            //for each one of the ingredient categories chose one random ingredient and populate the ingredientsArray with it
             ingredientsArray.push(pantry[userPreference][ingredientNumber]);
         }
     }
     return ingredientsArray;
 };
 
-/* Step 2 - define the functions which are going to use the main objects of the app */
-
-//changes the ingredient names from whichever case the are to "Title Case"
 var toTitleCase = function (str) {
     // "/\w\S*/g" is a regular expression (http://www.regular-expressions.info/) which searches for all words in a phrase ignoring the spaces
     return str.replace(/\w\S*/g, function (txt) {
@@ -102,15 +96,12 @@ function checkDropDowns(pantrySelection, dropdownvalue) {
 
 $(document).ready(function () {
 
-    //by default the output container is empty
     $('.output').hide();
 
     $('.orderBtn').on('click', function () {
 
-        //set the empty orderValues array
         var orderValues = [];
 
-        //check if the each one of the ingredient types have been chosen and that to the orderValues array;
         $('select').each(function () {
             if ($(this).val() === 'yes') {
                 orderValues.push(true);
@@ -124,26 +115,19 @@ $(document).ready(function () {
         concoction = new Drink(pantry, drinkOrder); // mix drink with Drink constructor
 
 
-        //if there is at least one ingredient selected then show the concoction
         if (concoction.length > 0) {
 
-            //build the chosen ingredients from the ingredients array
             var buildTheHtmlOutput = "";
             $.each(concoction, function (key, value) {
                 buildTheHtmlOutput += "<li>" + value + "</li>";
             });
 
-            //show the output container
             $('.output').show();
 
-            // name the customer's beverage with drinkNamer();
             $(".output h3").html("Here be yer Sparkly " + drinkNamer(concoction) + " Grog, ye scurvy dog!");
 
-            //populate it with the ingredients
             $(".output ul").html(buildTheHtmlOutput);
-        }
-        //if there are no ingredients selected then show alert
-        else {
+        } else {
             alert('Pick something for your poison!');
         }
         checkDropDowns(pantrySelection, dropdownvalue);
